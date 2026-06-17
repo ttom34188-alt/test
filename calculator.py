ed3dbe859eead9a4191ac603d9d38fece338be96
Lizhong Tang - dotSource SE <ltn@intranet.dotsource.de>
2026-06-17 16:24:58 +0200

    Add square_root function to calculator

--- calculator.py
+++ calculator.py
@@ -15,3 +15,8 @@ def divide(a, b):
 
 def power(a, b):
     return a ** b
+
+def square_root(a):
+    if a < 0:
+        raise ValueError("Cannot calculate square root of negative number")
+    return a ** 0.5
